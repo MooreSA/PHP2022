bd4c94f1ea12ec4e831e481d20a0df28a0721b86
Matt Redmond <redmondmj@gmail.com>
2022-01-13 12:45:33 -0400

    Intro! Setup! Test!

---
+++ 1_IntroToPHP/errorScript.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Making an Oops</title>
+    </head>
+    <body>
+        <?php
+            //the missing ; on the end of the first line causes an error.
+            echo "<p>I am trying to produce an error</p>"
+            echo "<p>Was I successful?</p>";
+        ?>
+    </body>
+</html>
