73256300ff50e31c4b85e7c606663ac56417e346
Seamus Moore <mooresa0@gmail.com>
2022-02-09 09:06:26 -0400

    Removed old code

--- 8_MyMusicApp/db_connect.php
+++ 8_MyMusicApp/db_connect.php
@@ -2,14 +2,6 @@
     
 $config = parse_ini_file("config.ini", true);
 
-// var_dump($config["database"]);
-
-// var_dump($config["database"]["DB_HOST"]);
-// var_dump($config["database"]["DB_USER"]);
-// var_dump($config["database"]["DB_PASS"]);
-// var_dump($config["database"]["DB_NAME"]);
-
-
 $con = new mysqli(
     $config["database"]["DB_HOST"], 
     $config["database"]["DB_USER"], 
